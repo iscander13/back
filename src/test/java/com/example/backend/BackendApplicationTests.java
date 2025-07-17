15ab2057ee2145e19c325153fd8c295685813d75
InkonIo <loke90@bk.ru>
2025-07-17 12:30:34 +0500

    12345678

--- src/test/java/com/example/backend/BackendApplicationTests.java
+++ src/test/java/com/example/backend/BackendApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.backend.config.TestMailConfig; // Импортируем вашу новую тестовую конфигурацию
+
+@SpringBootTest(classes = TestMailConfig.class) // <--- Указываем, какие классы конфигурации использовать
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Этот тест просто проверяет, успешно ли загружается контекст Spring.
+    }
 }
